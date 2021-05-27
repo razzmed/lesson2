2c231878f607377ab0905b5586f38dac93e95b32
razzmed <mederrazzakov@gmail.com>
2021-05-27 19:56:42 +0600

    lesson 2

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    int m;
+
+    public static void main(String[] args) {
+        
+        int d = printIt(45, 55, 66);
+        System.err.println(d);
+
+    }
+
+    public static int printIt(int a, int b, int c) {
+
+        int result = a + b + c;
+
+
+
+        return result;
+
+
+    }
+}
